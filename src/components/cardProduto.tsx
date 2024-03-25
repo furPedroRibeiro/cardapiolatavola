ca1a0892061a2163597a68a64bb67816f6f2f13d
furPedroRibeiro <pedrocodejob@gmail.com>
2024-03-25 14:04:58 -0300

    cards de produto atualizados

--- src/components/cardProduto.tsx
+++ src/components/cardProduto.tsx
@@ -11,9 +11,9 @@ export function CardProduto(props: PropsCard){
       className="flex flex-col justify-center items-center px-3 py-2 gap-2 h-[300px] w-[250px] border border-stone-400 rounded-xl">
       <img src={props.image} alt="" width={100} className=""/>
       <div className="flex flex-col items-center justify-center gap-1">
-        <h2 className="font-bold text-lg text-center">{props.title}</h2>
-        <p className="text-stone-800 text-lg text-center">{props.desc}</p>
-        <p className="text-lg text-center font-semibold italic bg-red-600 rounded-full text-white px-4 py-1">{props.price}</p>
+        <h2 className="font-bold text-lg text-center text-red-600">{props.title}</h2>
+        <p className="text-stone-800 text-base text-center">{props.desc}</p>
+        <p className="text-lg text-center font-semibold italic">{props.price}</p>
       </div>
     </div>
   )
