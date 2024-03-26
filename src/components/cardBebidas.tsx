3e46117cc96c6baf2a05e871d1fffb1b9e672d51
furPedroRibeiro <pedrocodejob@gmail.com>
2024-03-26 10:05:44 -0300

    cards atualizados - estilo

--- src/components/cardBebidas.tsx
+++ src/components/cardBebidas.tsx
@@ -8,12 +8,12 @@ interface PropsCard{
 export function CardBebida(props: PropsCard){
   return(
     <div
-      className="flex flex-col justify-center items-center px-3 py-2 gap-2 h-[300px] w-[250px] border border-stone-400 rounded-xl">
+      className="flex flex-col justify-center items-center px-3 py-2 gap-2 h-[300px] w-[250px] border border-stone-400 rounded-xl shadow-xl">
       <img src={props.image} alt="" width={100} className=""/>
       <div className="flex flex-col items-center justify-center gap-1">
         <h2 className="font-bold text-lg text-center text-red-600">{props.title}</h2>
-        <p className="text-stone-800 text-base text-center">{props.desc}</p>
-        <p className="text-base text-center font-semibold italic">{props.price}</p>
+        <p className="text-black text-base text-center">{props.desc}</p>
+        <p className="text-base text-center font-semibold italic text-stone-500">{props.price}</p>
       </div>
     </div>
   )
--- src/components/cardPizzas.tsx
+++ src/components/cardPizzas.tsx
@@ -10,23 +10,23 @@ interface PropsCard{
 export function CardPizza(props: PropsCard){
   return(
     <div
-      className="flex flex-col justify-center items-center px-3 py-2 gap-3 h-[320px] w-[250px] border border-stone-400 rounded-xl">
+      className="flex flex-col justify-center items-center px-3 py-2 gap-3 h-[320px] w-[250px] border border-stone-400 rounded-xl shadow-xl">
       <img src={props.image} alt="" width={100} className=""/>
       <div className="flex flex-col items-center justify-center gap-2">
         <h2 className="font-bold text-lg text-center text-red-600">{props.title}</h2>
-        <p className="text-stone-800 text-base text-center">{props.desc}</p>
+        <p className="text-black text-base text-center">{props.desc}</p>
         <div className="flex items-center justify-center flex-wrap gap-2">
           <div className="flex flex-col">
-            <p className="text-base text-center font-semibold italic">P</p>
-            <p className="text-base text-center font-semibold italic">{props.priceP}</p>
+            <p className="text-base text-center font-semibold italic text-stone-500">P</p>
+            <p className="text-base text-center font-semibold italic text-stone-500">{props.priceP}</p>
           </div>
           <div className="flex flex-col">
-            <p className="text-base text-center font-semibold italic">M</p>
-            <p className="text-base text-center font-semibold italic">{props.priceM}</p>
+            <p className="text-base text-center font-semibold italic text-stone-500">M</p>
+            <p className="text-base text-center font-semibold italic text-stone-500">{props.priceM}</p>
           </div>
           <div className="flex flex-col">
-            <p className="text-base text-center font-semibold italic">G</p>
-            <p className="text-base text-center font-semibold italic">{props.priceG}</p>
+            <p className="text-base text-center font-semibold italic text-stone-500">G</p>
+            <p className="text-base text-center font-semibold italic text-stone-500">{props.priceG}</p>
           </div>
         </div>
       </div>
